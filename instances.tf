dcafb1718330063043ce2380325decf5dd2947bc
Tom Chisholm <tomc@ignw.io>
2019-10-27 11:16:35 -0700

    fixing bug in in ingress rules

--- instances.tf
+++ instances.tf
@@ -28,6 +28,16 @@ resource "aws_security_group" "ssh_in" {
   description = "Highly insecure SG permitting SSH"
   name        = "allow-ssh-sg"
   vpc_id      = "${module.vpc-west.vpc_id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
