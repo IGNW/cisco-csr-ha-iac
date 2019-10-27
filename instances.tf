786b4af11dad85d104b4bf51bc8479a797d289b0
Tom Chisholm <tomc@ignw.io>
2019-10-27 11:20:53 -0700

    another bug in ingress

--- instances.tf
+++ instances.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "ssh_in" {
     # TLS (change to whatever ports you need)
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["0.0.0.0/0"]
