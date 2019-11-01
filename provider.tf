0dd29fc0cb95b55583945ced76853540e6349e1b
Tom Chisholm <tomc@ignw.io>
2019-11-01 07:34:28 -0700

    changing location of provider creds

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
+#provider "aws" {
+#  region = "us-west-2"
+#}
+
+
 provider "aws" {
-  region = "us-west-2"
+  region                  = "us-west-2"
+  shared_credentials_file = "/Users/.aws/creds"
+  profile                 = "default"
 }
