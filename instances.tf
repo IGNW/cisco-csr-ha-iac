235f2b4a2d0db03c7b1eeaab4ad6a7187092d63f
Tom Chisholm <tomc@ignw.io>
2019-11-12 11:08:04 -0800

    change machine size

--- instances.tf
+++ instances.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "ssh_in" {
 
 resource "aws_instance" "csr" {
   ami           = "${data.aws_ami.csr.id}"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
 
   tags = {
     Project = "CSR HA",
