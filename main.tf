aaf3e476f1503e356c98f60b44f26cdd1733bf9c
Tyler Walker <tylerw@ignw.io>
2019-12-04 11:38:23 -0600

    used data retrieval method for putting the ami into the config

--- main.tf
+++ main.tf
@@ -105,7 +105,8 @@ module instance1 {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> 2.0"
   #ami = "cisco-CSR-.16.12.01a-BYOL-HVM-2-624f5bb1-7f8e-4f7c-ad2c-03ae1cd1c2d3-ami-0a35891127a1b85e1.4" 
-  ami = "ami-0fc7a3d5400f4619d"
+  #ami = "ami-0fc7a3d5400f4619d"
+  ami = "${data.aws_ami.csr1000v.id}"
   instance_type          = "t2.micro"
   name = "csr1000v1"
   key_name = "csr1000v"
@@ -131,10 +132,27 @@ module instance1 {
   
 }
 
+data "aws_ami" "csr1000v" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["cisco-CSR-.16.12.01a-BYOL-HVM-2-624f5bb1-7f8e-4f7c-ad2c-03ae1cd1c2d3-ami-0a35891127a1b85e1.4"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["679593333241"] # Cisco
+
+}
+
 module instance2 {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> 2.0"
-  ami = "ami-0fc7a3d5400f4619d"
+  ami = "${data.aws_ami.csr1000v.id}"
   name = "csr1000v2"
   key_name = "csr1000v"
   instance_type          = "t2.micro"
