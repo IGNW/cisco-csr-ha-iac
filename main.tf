6d6d390922f5f7fcef38e3eb87ac5279a61409fd
Tyler Walker <tylerw@ignw.io>
2019-12-26 14:39:36 -0600

    Reassigning public ips to both routers

--- main.tf
+++ main.tf
@@ -222,7 +222,7 @@ module instance1 {
   name = "csr1000v1"
   key_name = "csr"
   iam_instance_profile = "${aws_iam_instance_profile.csr1000v.name}"
-  #associate_public_ip_address = true
+  associate_public_ip_address = true
   vpc_security_group_ids = ["${module.security_group_outside.this_security_group_id}", "${module.ssh_security_group.this_security_group_id}"]
 }
 
@@ -246,7 +246,7 @@ data "aws_ami" "csr1000v" {
 module instance2 {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> 2.0"
-  #associate_public_ip_address = true
+  associate_public_ip_address = true
   ami = "${data.aws_ami.csr1000v.id}"
   name = "csr1000v2"
   key_name = "csr"
