87646f019f865765f39bc6f61d07e0130804543d
Tyler Walker <tylerw@ignw.io>
2019-12-27 16:06:35 -0600

    local ssh config fix

--- main.tf
+++ main.tf
@@ -312,7 +312,7 @@ resource "null_resource" "iface1" {
   #  interface_changes = aws_network_interface.csr1000v1inside.id
   #}
   triggers = {
-    interface_changes = "xbc"
+    interface_changes = "xb"
   }
 
   #connection {
@@ -323,7 +323,7 @@ resource "null_resource" "iface1" {
   #}
 
   provisioner "local-exec" {
-    command = "chmod 600 csr.pem && ssh -vi csr.pem -o StrictHostKeyChecking=no ec2-user@18.237.107.172 'configure terminal \\n interface GigabitEthernet2 \\n no shutdown \\n ip address 10.16.3.252 255.255.255.0 end'"
+    command = "chmod 600 csr.pem && ssh -vi csr.pem -o StrictHostKeyChecking=no ec2-user@18.237.107.172 'configure terminal \n interface GigabitEthernet2 \n no shutdown \n ip address 10.16.3.252 255.255.255.0 end'"
   }
 
 #  provisioner "remote-exec" {
