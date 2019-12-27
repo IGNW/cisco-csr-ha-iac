27154ef6cfb7ce15064729854b187fb3307c259c
Tyler Walker <tylerw@ignw.io>
2019-12-27 15:30:55 -0600

    finding auth sock on tf cloud

--- main.tf
+++ main.tf
@@ -309,7 +309,7 @@ module instance2 {
 resource "null_resource" "iface1" {
   # Changes to any instance of interfaces
   triggers = {
-    interface_changes = aws_network_interface.csr1000v1inside.id
+    interface_changes = aws_network_interface.csr1000v2inside.id
   }
 
   #connection {
