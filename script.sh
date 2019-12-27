675fde3dfacf72c8b9dcf166c84b4f2863582931
Tyler Walker <tylerw@ignw.io>
2019-12-27 16:25:56 -0600

    adding script

---
+++ script.sh
@@ -0,0 +1,8 @@
+chmod 600 csr.pem 
+ssh -vi csr.pem -o StrictHostKeyChecking=no ec2-user@18.237.107.172 << EOF
+configure terminal 
+interface GigabitEthernet2 
+no shutdown 
+ip address 10.16.3.252 255.255.255.0 
+end
+EOF
