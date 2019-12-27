76644530e859a1381fe3c224eb283ca690b1be15
Tyler Walker <tylerw@ignw.io>
2019-12-27 16:00:35 -0600

    local ssh config fix

--- main.tf
+++ main.tf
@@ -312,7 +312,7 @@ resource "null_resource" "iface1" {
   #  interface_changes = aws_network_interface.csr1000v1inside.id
   #}
   triggers = {
-    interface_changes = "xabc"
+    interface_changes = "xbc"
   }
 
   #connection {
