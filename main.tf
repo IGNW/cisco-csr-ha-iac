307a02360a8ac5f858b7c50d318d0f66c124a470
Tyler Walker <tylerw@ignw.io>
2019-12-27 15:46:37 -0600

    local ssh config fix

--- main.tf
+++ main.tf
@@ -323,7 +323,7 @@ resource "null_resource" "iface1" {
   #}
 
   provisioner "local-exec" {
-    command = "ssh -vi csr.pem -o StrictHostKeyChecking=no ec2-user@18.237.107.172 'configure terminal end interface GigabitEthernet2'"
+    command = "chmod 600 csr.pem && ssh -vi csr.pem -o StrictHostKeyChecking=no ec2-user@18.237.107.172 'configure terminal end interface GigabitEthernet2'"
   }
 
   provisioner "remote-exec" {
