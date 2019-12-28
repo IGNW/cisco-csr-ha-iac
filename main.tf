0be3430794b1bdc355bcc55f40931f34eaf0cb4c
Tyler Walker <tylerw@ignw.io>
2019-12-27 18:25:47 -0600

    updated ami

--- main.tf
+++ main.tf
@@ -295,7 +295,7 @@ module instance2 {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> 2.0"
   associate_public_ip_address = true
-  ami = "${data.aws_ami.csr1000v.id}"
+  #ami = "${data.aws_ami.csr1000v.id}"
   ami = "ami-0384153b9e3387f59"
   name = "csr1000v2"
   key_name = "csr"
