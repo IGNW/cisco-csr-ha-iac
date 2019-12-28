122b65567e9fd0de93166ccc60f32368cc4753b5
Tyler Walker <tylerw@ignw.io>
2019-12-27 19:38:48 -0600

    updated script to wait

--- main.tf
+++ main.tf
@@ -316,7 +316,7 @@ resource "null_resource" "iface1" {
   }
 
   provisioner "local-exec" {
-    command = "chmod 700 script.sh && ./script.sh"
+    command = "sleep 180 && chmod 700 && script.sh && ./script.sh"
   }
 
 }
