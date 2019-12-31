6a189dade6d6e4ec117d22403b9e118f5c0d51c3
Tyler Walker <tylerw@ignw.io>
2019-12-31 00:59:38 -0600

    guestshell bug fix

--- main.tf
+++ main.tf
@@ -288,7 +288,7 @@ resource "null_resource" "iface1" {
   # Changes to any instance of interfaces
   triggers = {
     #interface_changes = aws_network_interface.csr1000v1eth1.id
-    interface_changes = "ass;dlkfj"
+    interface_changes = "lkfj"
   }
 
   provisioner "local-exec" {
