154a89465c6a760d8cb141eb407c03f5c8b01732
Tyler Walker <tylerw@ignw.io>
2020-01-09 11:58:35 -0600

    updated for public

--- key.tf
+++ key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "csrkey" {
-  key_name   = "cisco_router_ssh_key"
-  public_key = base64decode("${var.base64encoded_public_ssh_key}")
+  key_name    = "cisco_router_ssh_key"
+  public_key = "${base64decode("${var.base64encoded_public_ssh_key}")}"
 }
