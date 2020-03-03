7416fd8bf8bdfaddfdcb91d8f3d8acdf65c00b1b
Tyler Walker <tylerw@ignw.io>
2020-03-03 13:11:22 -0600

    removed deprecated switch

--- iam.tf
+++ iam.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_instance_profile" "csr1000v" {
-  count = "${var.csr1000v_instance_profile != "" ? 1 : 0}"
+  count = var.csr1000v_instance_profile
   name  = "csr1000v"
   role  = "${aws_iam_role.csr_role[0].name}"
 }
 
 resource "aws_iam_policy" "csrpolicy" {
-  count       = "${var.csr1000v_instance_profile != "" ? 1 : 0}"
+  count       = var.csr1000v_instance_profile
   name        = "csr_policy"
   path        = "/"
   description = "My test policy"
@@ -44,14 +44,14 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "csr-attach" {
-  count      = "${var.csr1000v_instance_profile != "" ? 1 : 0}"
+  count      = var.csr1000v_instance_profile
   role       = "${aws_iam_role.csr_role[0].name}"
   policy_arn = "${aws_iam_policy.csrpolicy[0].arn}"
 }
 
 
 resource "aws_iam_role" "csr_role" {
-  count                = "${var.csr1000v_instance_profile != "" ? 1 : 0}"
+  count                = var.csr1000v_instance_profile
   name                 = "csr1000v"
   path                 = "/"
   permissions_boundary = "${aws_iam_policy.csrpolicy[0].arn}"
