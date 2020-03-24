9874b7b9ecb333953272499ed39dfc0a226ec521
Tyler Walker <tylerw@ignw.io>
2020-03-24 17:21:35 -0500

    removed profile output

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,6 @@ output "node2_public_ip_address" {
   value = "${module.instance2.public_ip}"
 }
 
-output "csr1000v_instance_profile" {
-  value = "${aws_iam_instance_profile.csr1000v[0].name}"
-}
-
 output "output_script" {
   value = local.output_script
 }
