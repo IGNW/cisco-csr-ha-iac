fcb85f052475a5039f4dc58bedba68379e475f4d
Tyler Walker <tylerw@ignw.io>
2020-03-24 16:45:09 -0500

    added fixes to output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "node1_public_ip_address" {
   value = "${module.instance1.public_ip}"
 }
+
 output "node2_public_ip_address" {
   value = "${module.instance2.public_ip}"
 }
+
 output "csr1000v_instance_profile" {
   value = "${aws_iam_instance_profile.csr1000v[0].name}"
 }
